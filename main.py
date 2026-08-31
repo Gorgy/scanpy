560af66f12539da1f019e1eee72253f03892bccc
Belov Alexey <belovaa@lad24.ru>
2026-08-31 14:32:15 +0300

    Add day 4 collection exercises

--- main.py
+++ main.py
@@ -49,7 +49,18 @@ def create_stats():
         "error_stats": {},
         "unique_events_ids": set(),
         "events_by_device": {},
+        "events_by_type": {},
     }
+
+
+def get_duplicate_event_ids(events_list):
+    dict_events = {}
+    for event in events_list:
+        if event["event_id"] not in dict_events:
+            dict_events[event["event_id"]] = 1
+        else:
+            dict_events[event["event_id"]] += 1
+    return {key for key, value in dict_events.items() if value > 1}
 
 
 def validate_event(event_item):
@@ -99,13 +110,30 @@ def update_error_stats(error_stats, errors_list):
         error_stats[error_item] += 1
 
 
-def update_events_by_device(stats_dict, event_item):
-    event_by_device = stats_dict["events_by_device"]
+def update_events(stats_dict_item, event_item):
+    if event_item:
+        if event_item not in stats_dict_item:
+            stats_dict_item[event_item] = 0
+        stats_dict_item[event_item] += 1
 
-    if event_item["device_id"]:
-        if event_item["device_id"] not in event_by_device:
-            event_by_device[event_item["device_id"]] = 0
-        event_by_device[event_item["device_id"]] += 1
+
+def get_barcodes(events_list):
+    return [event["barcode"] for event in events_list if event["barcode"]]
+
+
+def get_unique_barcodes(events_list):
+    return {event["barcode"] for event in events_list if event["barcode"]}
+
+
+def get_events_dict_by_event_id(events_list):
+    return {event["event_id"]: event for event in events_list}
+
+
+def print_barcodes(barcodes_list, title="Штрихкоды:"):
+    if len(barcodes_list) != 0:
+        print_header(title)
+        for barcode in barcodes_list:
+            print(f"- {barcode}")
 
 
 def print_error_stats(stats_dict):
@@ -118,6 +146,7 @@ def print_error_stats(stats_dict):
 
 def print_events_by_device(stats_dict):
     events_by_device = stats_dict["events_by_device"]
+    events_by_type = stats_dict["events_by_type"]
     events_ids = stats_dict["unique_events_ids"]
 
     print(f"\nУникальных событий: {len(events_ids)}")
@@ -126,11 +155,17 @@ def print_events_by_device(stats_dict):
     for key, value in events_by_device.items():
         print(f"{key}: {value}")
 
+    if events_by_type:
+        print_header("Событий по типу:")
+    for key, value in events_by_type.items():
+        print(f"{key}: {value}")
+
 
 def process_event(event_item, stats_dict):
     event_errors = validate_event(event_item)
     stats_dict["unique_events_ids"].add(event_item["event_id"])
-    update_events_by_device(stats_dict, event_item)
+    update_events(stats_dict["events_by_device"], event_item["device_id"])
+    update_events(stats_dict["events_by_type"], event_item["event_type"])
 
     if event_errors:
         stats_dict["rejected_events"] += 1
@@ -143,6 +178,17 @@ def process_event(event_item, stats_dict):
         print_event(event_item)
 
 
+def print_duplicate_events(duplicate_ids):
+    if len(duplicate_ids) > 0:
+        print(f"Дубликаты: {duplicate_ids}")
+    else:
+        print("Дубликатов нет")
+
+
+def print_events_len(events_dict):
+    print(f"Событий по ID: {len(events_dict)}")
+
+
 def process_events(event_list, stats_dict):
     for event in event_list:
         stats_dict["total_events"] += 1
@@ -150,8 +196,17 @@ def process_events(event_list, stats_dict):
 
 
 stats = create_stats()
+barcodes = get_barcodes(events)
+barcodes_unique = get_unique_barcodes(events)
+events_dict = get_events_dict_by_event_id(events)
+duplicate = get_duplicate_event_ids(events)
 
 process_events(events, stats)
 print_stats(stats)
 print_error_stats(stats)
 print_events_by_device(stats)
+print_barcodes(barcodes)
+print_barcodes(barcodes_unique, "Уникальные штрихкоды")
+print_events_len(events_dict)
+print_duplicate_events(duplicate)
+print_duplicate_events({})
